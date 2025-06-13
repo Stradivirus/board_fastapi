6c6a46636207a20c89ebf7d16a44c9e4fca8b21a
stradivirus <stradivirus9@gmail.com>
2025-06-13 10:48:00 +0900

    [up] url 오타

--- board_rv/src/api/urls.ts
+++ board_rv/src/api/urls.ts
@@ -1,5 +1,5 @@
 // const API_BASE = "http://localhost:8000/api";
-const API_BASE = "http://http://141.147.144.151:8001/api";
+const API_BASE = "http://141.147.144.151:8001/api";
 
 export const API_URLS = {
     POSTS: `${API_BASE}/posts`,
